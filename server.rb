b33a091d925953d575fecfe776870252cdef95e1
Michael Montero <miguelmmm809@gmail.com>
2016-04-30 16:20:05 -0400

    added a button and search bar

--- server.rb
+++ server.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 require './models/siskel.rb'
 
+#
+# get '/' do
+#   erb :index
+# end
 
 get '/' do
+  title = params[:title]
+  @movie = Siskel.new(title)
   erb :index
-end
-get '/movie' do
-  movie = Siskel.new("Lion King")
-  "This movie title is #{movie.title}"
 end
